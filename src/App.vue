73cf9678d32b5710d86ed8b8574d8365b5bb5fca
Diego Alejandro Reyes Espitia <diego.reyes@tars.dev>
2020-01-16 09:59:05 -0500

    introduction to fetch and api, video 29

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <main></main>
+  <main>
+    <px-header />
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
+  </main>
 </template>
 
 <script>
+import PxHeader from "@/components/PxHeader";
+
 export default {
   name: "app",
-  components: {}
+  components: { PxHeader }
 };
 </script>
 
