18f73954cf9030ab1bf0f815a42bd18a6f41ad48
diego.reyes <diego.reyes@tars.dev>
2020-01-17 16:07:50 -0500

    done video 34

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css";
-import router from "@/router";
 
+import router from "@/router";
+import { dollarFilter, percentFilter } from "@/filters";
+import Chartick from "vue-chartkick";
+import Chart from "chart.js";
+import { VueSpinners } from "@saeris/vue-spinners";
+
+Vue.use(VueSpinners);
+Vue.use(Chartick.use(Chart));
+
+Vue.filter("dollar", dollarFilter);
+Vue.filter("percent", percentFilter);
 Vue.config.productionTip = false;
 
 new Vue({
